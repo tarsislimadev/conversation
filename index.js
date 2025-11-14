9dc349b9f8e11b7a292c35b9531ad96615f0af61
Tarsis Lima <br.tmvdl@gmail.com>
2025-11-14 18:03:31 -0300

    Update project files

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
+import { GoogleGenAI } from 'https://cdn.jsdelivr.net/npm/@google/genai@1.29.1'
+
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 document.body.style.margin = '0px'
 
-const getWidth = () => 450
-const getHeight = () => getWidth() * 4 / 3
-
-const videos = document.getElementById('videos')
-videos.style.display = 'flex'
-videos.style.justifyContent = 'space-between'
-
-const robot = document.getElementById('robot')
-robot.style.backgroundColor = '#000000'
-
-const human = document.getElementById('human')
-const video = document.createElement('video')
-video.style.height = getHeight() + 'px'
-video.style.width = getWidth() + 'px'
-video.autoplay = true
-video.muted = true
-video.style.backgroundColor = '#000000'
-human.appendChild(video)
-
-const start = document.getElementById('start')
+const getWidth = () => window.innerWidth
+const getHeight = () => window.innerHeight
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(getHeight(), getWidth())
-const onAnimationLoop = () => renderer.setAnimationLoop(() => renderer.render(scene, camera))
-robot.appendChild(renderer.domElement)
+renderer.setSize(getWidth(), getHeight())
+renderer.setAnimationLoop(() => renderer.render(scene, camera))
+document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(45, getHeight() / getWidth(), 1, 1000)
+const camera = new THREE.PerspectiveCamera(45, getWidth() / getHeight(), 1, 1000)
 camera.position.z = 120
 
 scene.add(new THREE.AmbientLight(0x666666))
@@ -68,8 +52,38 @@ gltfLoader.load('models/gltf/LeePerrySmith/LeePerrySmith.glb', (gltf) => {
 
 //
 
-start.addEventListener('click', async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-  video.srcObject = stream
-  onAnimationLoop()
-})
+const getSearch = (name, def = '') => {
+  const url = new URL(window.location)
+  const param = url.searchParams.get(name)
+  return param || def
+}
+
+const GEMINI_API_KEY = getSearch('GEMINI', '')
+
+const VOICE_API_KEY = getSearch('VOICE', '')
+
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY })
+
+const getVoiceURL = (text) => `https://api.voicerss.org/?key=${VOICE_API_KEY}&hl=en-us&src=` + encodeURIComponent(text)
+
+//
+
+const model = 'gemini-2.5-pro'
+
+const onInputValue = async () => {
+  const contents = input.value
+  input.value = ''
+  const response = await ai.models.generateContent({ model, contents })
+
+  const text = response.text
+  console.log({ text })
+
+  const audio = document.createElement('audio')
+  audio.autoplay = true
+  audio.src = getVoiceURL(text)
+  document.body.append(audio)
+}
+
+const input = document.getElementById('input')
+
+input.addEventListener('keyup', (ev) => (ev.key == 'Enter') && onInputValue())
